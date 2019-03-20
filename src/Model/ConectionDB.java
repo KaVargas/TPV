9fb37d51583ba8da4203c8f82ab11bd54da479f1
Kenedy Vargas <35229437+KaVargas@users.noreply.github.com>
2019-03-19 19:23:08 -0500

    Conection to DB

--- src/Model/ConectionDB.java
+++ src/Model/ConectionDB.java
@@ -5,10 +5,43 @@
  */
 package Model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author kened
  */
 public class ConectionDB {
-    
+    //------Atributos de la clase----------
+    String jdbcDriver = ("com.mysql.jdbc.Driver");//drivers
+    String jdbcSup = ("mysql");//base de datos
+    String jdbcHost = ("localhost");//servidor
+    String jdbcBd = ("capp");//nombre de la base de datos
+    String jdbcUser = ("root");//usuario
+    String jdbcPwd = ("12345678");//pongo mi clave
+    Connection con = null;
+
+    public ConectionDB() {
+    }
+
+    public Connection conectionBD() {
+
+        try {
+            Class.forName(jdbcDriver);
+            String strcon = "jdbc:" + jdbcSup + "://" + jdbcHost + "/" + jdbcBd;
+            con = DriverManager.getConnection(strcon, jdbcUser, jdbcPwd);
+            return con;
+        } catch (ClassNotFoundException ex) {
+            JOptionPane.showMessageDialog(null, ex.getMessage(), "mensaje fallo de conexion", JOptionPane.ERROR_MESSAGE);
+            System.out.println("Driver Incorrecto" + ex.getMessage());
+            return null;
+        } catch (SQLException exsql) {
+            JOptionPane.showMessageDialog(null, exsql.getMessage(), "mensaje", JOptionPane.ERROR_MESSAGE);
+            System.out.println("BDD INCORRECTA" + exsql.getMessage());
+            return null;
+        }
+    }
 }
